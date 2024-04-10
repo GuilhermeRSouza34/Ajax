accf2a79549c599fcb7e80b68f6c505016fdfb72
Guilherme <guilhermersouza34@gmail.com>
2024-04-10 17:06:42 -0300

    Criando com o JQuery

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('btn-buscar-cep').addEventListener('click', function() {
-        const xhttp = new XMLHttpRequest();
-        const cep = document.getElementById('cep').value;
-        const endpoint = `https://viacep.com.br/ws/${cep}/json`;
-
-        xhttp.open('GET', endpoint);
-        xhttp.send();
-    })
-})
+//document.addEventListener('DOMContentLoaded', function() {
+//    document.getElementById('btn-buscar-cep').addEventListener('click', function() {
+//        const xhttp = new XMLHttpRequest();
+//        const cep = document.getElementById('cep').value;
+//        const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+//
+//        xhttp.open('GET', endpoint);
+//        xhttp.send();
+//    })
+//})
 
 
 $(document).ready(function () {
     $('#btn-buscar-cep').click(function() {
         const cep = $('cep').val();
         const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+
+        $.ajax(endpoint).done(function(resposta) {
+            console.log(resposta);
+        })
     })
 })
