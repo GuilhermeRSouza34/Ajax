5f44cd30ec9d51aad65cb94960eaf2b74198834b
Guilherme <guilhermersouza34@gmail.com>
2024-04-10 16:55:17 -0300

    Atualizando o JS

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('btn-buscar-cep').addEventListener('click', function(){
+    document.getElementById('btn-buscar-cep').addEventListener('click', function() {
         const xhttp = new XMLHttpRequest();
-        const cep = document.createElement('cep').value;
+        const cep = document.getElementById('cep').value;
         const endpoint = `https://viacep.com.br/ws/${cep}/json`;
 
         xhttp.open('GET', endpoint);
