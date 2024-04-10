609a59d0690b6690aa372d7eb825abedeeb039ec
Guilherme <guilhermersouza34@gmail.com>
2024-04-10 16:48:39 -0300

    Utilizando o xhttp

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('btn-buscar-cep').addEventListener('click', function(){
         const xhttp = new XMLHttpRequest();
         const cep = document.createElement('cep').value;
-        const endpoint = `https://viacep.com.br/ws/${cep}/json`
+        const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+
+        xhttp.open('GET', endpoint);
+        xhttp.send();
     })
 })
