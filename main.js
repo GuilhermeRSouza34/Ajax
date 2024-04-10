63da9c57e974787612d74c28e37e47b2d49d4edd
Guilherme <guilhermersouza34@gmail.com>
2024-04-10 16:46:04 -0300

    RequestXML

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('btn-buscar-cep').addEventListener('click', function(){
         const xhttp = new XMLHttpRequest();
+        const cep = document.createElement('cep').value;
+        const endpoint = `https://viacep.com.br/ws/${cep}/json`
     })
 })
